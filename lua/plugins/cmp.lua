fa699ae86208866e5922511c37da0ab64fdba90f
mugamma <mghavami@mit.edu>
2026-09-17 10:14:05 -0400

    remove tab completion for cmp for easier copilot use

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -19,7 +19,7 @@ return {
           ["<C-Space>"] = cmp.mapping.complete(),
           ["<C-e>"] = cmp.mapping.abort(),
           ["<CR>"] = cmp.mapping.confirm({ select = true }),
-          ["<Tab>"] = cmp.mapping.select_next_item(),
+          ["<C-n>"] = cmp.mapping.select_next_item(),
           ["<S-Tab>"] = cmp.mapping.select_prev_item(),
         }),
         sources = cmp.config.sources({
